2e06e39886dd40dac752b89952db7643aeab6ce6
kpbaks <kristoffer.pbs@tuta.io>
2023-10-06 13:51:00 +0200

    feat: add start of gaussian cuda kerel

---
+++ filters.cuh
@@ -0,0 +1,24 @@
+#pragma once
+
+// #include <cuda_runtime.h>
+#include <device_launch_parameters.h>
+
+__global__ void cuda_gaussian_blur(const unsigned char* input, unsigned char* output, const double* kernel, int width, int height, int kernel_size) {
+    int x = blockIdx.x * blockDim.x + threadIdx.x;
+    int y = blockIdx.y * blockDim.y + threadIdx.y;
+
+    if(x < width && y < height) {
+        double value = 0.0;
+        int offset = kernel_size / 2;
+
+        for(int j = -offset; j <= offset; j++) {
+            for(int i = -offset; i <= offset; i++) {
+                int x_ = min(max(x + i, 0), width - 1);
+                int y_ = min(max(y + j, 0), height - 1);
+                value += input[y_ * width + x_] * kernel[(j + offset) * kernel_size + (i + offset)];
+            }
+        }
+
+        output[y * width + x] = static_cast<unsigned char>(value);
+    }
+}
--- main.cu
+++ main.cu
@@ -1,5 +1,7 @@
 #include <cstdarg>
 #include <cstdio>
+#include <cuda_runtime_api.h>
+#include <driver_types.h>
 #include <iostream>
 #include <string>
 #include <unistd.h>
@@ -7,39 +9,41 @@
 #define STB_IMAGE_IMPLEMENTATION
 #include "stb_image.h"
 
-namespace escape_codes {
-constexpr const char *reset = "\033[0m";
-constexpr const char *red = "\033[31m";
-constexpr const char *green = "\033[32m";
-constexpr const char *yellow = "\033[33m";
-constexpr const char *blue = "\033[34m";
-constexpr const char *magenta = "\033[35m";
-constexpr const char *cyan = "\033[36m";
-} // namespace escape_codes
+#include "common.hpp"
+#include "filters.cuh"
+// #include "filters.hpp"
 
-#ifndef NDEBUG
-#define DEBUG_LOG(stream, format, ...)                                         \
-  /* The do {...} while(0) structure is a common idiom used in macros. */      \
-  /* It allows the macro to be used in all contexts that a normal function     \
-   * call could be used. */                                                    \
-  /* It creates a compound statement in C/C++ that behaves as a single         \
-   * statement. */                                                             \
-  do {                                                                         \
-    bool is_tty = isatty(fileno(stream));                                      \
-    if (is_tty) {                                                              \
-      fprintf(stream, "%s%s%s:%s%s%s:%d: ", escape_codes::cyan, __FILE__,      \
-              escape_codes::reset, escape_codes::yellow, __func__,             \
-              escape_codes::reset, __LINE__);                                  \
-    } else {                                                                   \
-      fprintf(stream, "%s:%s:%d: ", __FILE__, __func__, __LINE__);             \
-    }                                                                          \
-    fprintf(stream, format, ##__VA_ARGS__);                                    \
-    fprintf(stream, "\n");                                                     \
-  } while (0)
-#else
-// do nothing
-#define DEBUG_LOG(stream, format, ...)
-#endif
+constexpr int KERNEL_SIZE = 5;
+constexpr double SIGMA = 1.0;
+
+double* generate_gaussian_kernel(int N) {
+    // std::array<std::array<double, N>, N> kernel;
+    double* kernel = new double[N * N];
+    // auto kernel = SquareMatrix<double, N>{};
+    double sum = 0.0;
+    int offset = KERNEL_SIZE / 2;
+    for (int y = -offset; y <= offset; y++) {
+        for (int x = -offset; x <= offset; x++) {
+            // double element = (1.0 / (2 * M_PI * SIGMA * SIGMA)) * std::exp(-(x * x + y * y) / (2 * SIGMA * SIGMA));
+            // kernel.set(x + offset, y + offset, element);
+            // sum += element;
+            int index = (y + offset) * N + (x + offset);
+            kernel[index] = (1.0 / (2 * M_PI * SIGMA * SIGMA)) * std::exp(-(x * x + y * y) / (2 * SIGMA * SIGMA));
+            sum += kernel[index];
+        }
+    }
+
+    // Normalize the kernel to make the sum of its coefficients equal to 1.
+    for (int y = 0; y < KERNEL_SIZE; y++) {
+        for (int x = 0; x < KERNEL_SIZE; x++) {
+            // kernel.set(x, y, kernel(x, y) / sum);
+            kernel[N * y + x] /= sum;
+        }
+    }
+
+    return kernel;
+}
+
 
 auto main(int argc, char **argv) -> int {
   if (argc != 2) {
@@ -59,11 +63,48 @@ auto main(int argc, char **argv) -> int {
   std::printf("Image %s: %dx%d, %d channels\n", filename.c_str(), width, height,
               channels);
 
-  DEBUG_LOG(stderr, "This is a debug message");
 
-  //
+  auto kernel = generate_gaussian_kernel(KERNEL_SIZE);
+  // flatten
+  // double* kernel = new double[KERNEL_SIZE * KERNEL_SIZE];
+  // for (int i = 0; i < KERNEL_SIZE; i++) {
+  //   for (int j = 0; j < KERNEL_SIZE; j++) {
+  //     kernel[i * KERNEL_SIZE + j] = kernel_mat[j][i];
+  //   }
+  // }
 
+  // CUDA things
+
+  unsigned char* d_output;
+  unsigned char* d_input;
+  double* d_kernel;
+
+  int size = width * height * channels * sizeof(unsigned char);
+  cudaMalloc(&d_input, size);
+  cudaMalloc(&d_output, size);
+  cudaMalloc(&d_kernel, KERNEL_SIZE * KERNEL_SIZE * sizeof(double));
+
+  cudaMemcpy(d_input, data, size, cudaMemcpyHostToDevice);
+  cudaMemcpy(d_kernel, kernel, KERNEL_SIZE * KERNEL_SIZE * sizeof(double), cudaMemcpyHostToDevice);
+
+  dim3 block(16, 16);
+  dim3 grid((width + block.x - 1) / block.x, (height + block.y - 1) / block.y);
+
+  cuda_gaussian_blur<<<grid, block>>>(d_input, d_output, d_kernel, width, height, KERNEL_SIZE);
+
+  unsigned char* output_data = new unsigned char[size];
+  cudaMemcpy(output_data, d_output, size, cudaMemcpyDeviceToHost);
+
+
+  auto output_filename = filename + ".out.png";
+  stbi_write_png(output_filename.c_str(), width, height, channels, output_data, width * channels);
   stbi_image_free(data);
+  delete [] kernel;
+  delete [] output_data;
+
+  cudaFree(d_input);
+  cudaFree(d_output);
+  cudaFree(d_kernel);
 
   return EXIT_SUCCESS;
 }
